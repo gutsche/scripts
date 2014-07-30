58056a082cba5fc3fe2ff4fe193ad9ed76e4b3be
Oliver Gutsche (oguatworld) <oguatworld@gmail.com>
2014-07-30 14:51:17 +0200

    added MINIAODSIM for users

--- Workflow_Progress/prepare_progress_plots_parallel.sh
+++ Workflow_Progress/prepare_progress_plots_parallel.sh
@@ -17,7 +17,9 @@ python EventsPerDay.py -d '/*/Fall13-*/GEN-SIM' -s 2013-09-06 &
 python EventsPerDay.py -d '/*/Spring14dr-*/AODSIM' -s 2014-04-30 &
 python EventsPerDay.py -d '/*/Spring14dr-*S14_POSTLS170*/AODSIM' -s 2014-03-30 &
 python EventsPerDay.py -d '/*/Spring14dr-*PU20bx25_POSTLS170*/AODSIM' -s 2014-03-30 &
+python EventsPerDay.py -d '/*/Spring14dr-*PU40bx25_POSTLS170*/AODSIM' -s 2014-07-17
 python EventsPerDay.py -d '/*/Spring14*/MINIAODSIM' -s 2014-06-20 &
+python EventsPerDay.py -m -d /*/*/USER -u https://cmsweb.cern.ch/dbs/prod/phys03/DBSReader -s 2014-06-20 &
 
 python EventsPerDay.py -d '/*/*Upg14*/AODSIM' -s 2014-04-27 &
 python EventsPerDay.py -d '/*/*Upg14*/GEN-SIM-RECO' -s 2014-04-30 &
@@ -33,6 +35,8 @@ python EventsPerDay.py -d '/*/*/AODSIM' -s 2014-04-05 &
 
 python EventsPerDay.py -d '/*/Summer12*/GEN-SIM' -s 2012-12-06
 
+python addJSONFiles.py -n User-MINIAOD -d STAR_STAR_USER.json,STAR_Spring14STAR_MINIAODSIM.json
+
 python DrawStackedPlot.py -d STAR_Fall13-STAR_GEN-SIM.json
 python DrawStackedPlot.py -d STAR_STARUpg14STAR_AODSIM.json
 python DrawStackedPlot.py -d STAR_STARUpg14STAR_GEN-SIM-RECO.json
@@ -40,8 +44,10 @@ python DrawStackedPlot.py -d STAR_STARUpg14STAR_GEN-SIM.json
 python DrawStackedPlot.py -d STAR_STAR_AODSIM.json
 python DrawStackedPlot.py -d STAR_STAR_GEN-SIM.json
 python DrawStackedPlot.py -d STAR_Spring14STAR_MINIAODSIM.json
+python DrawStackedPlot.py -d STAR_STAR_USER.json
 python DrawStackedPlot.py -d STAR_Spring14dr-STARPU20bx25_POSTLS170STAR_AODSIM.json
 python DrawStackedPlot.py -d STAR_Spring14dr-STARS14_POSTLS170STAR_AODSIM.json
+python DrawStackedPlot.py -d STAR_Spring14dr-STARPU40bx25_POSTLS170STAR_AODSIM.json
 python DrawStackedPlot.py -d STAR_Spring14dr-STAR_AODSIM.json
 python DrawStackedPlot.py -d STAR_Summer11STAR_AODSIM.json
 python DrawStackedPlot.py -d STAR_Summer11STAR_GEN-SIM.json
@@ -53,6 +59,10 @@ python DrawOverviewStackedPlots.py -a STAR_STAR_GEN-SIM.json -d STAR_Fall13-STAR
 
 python DrawOverviewStackedPlots.py -a STAR_STAR_AODSIM.json -d STAR_Spring14dr-STAR_AODSIM.json,STAR_STARUpg14STAR_AODSIM.json,STAR_Summer12STAR_AODSIM.json,STAR_Summer11STAR_AODSIM.json
 python DrawOverviewStackedPlots.py -a STAR_STAR_AODSIM.json -d STAR_Spring14dr-STAR_AODSIM.json,STAR_STARUpg14STAR_AODSIM.json,STAR_Summer12STAR_AODSIM.json,STAR_Summer11STAR_AODSIM.json -s valid
+
+python DrawOverviewStackedPlots.py -a STAR_Spring14dr-STAR_AODSIM.json -d STAR_Spring14dr-STARPU40bx25_POSTLS170STAR_AODSIM.json,STAR_Spring14dr-STARPU20bx25_POSTLS170STAR_AODSIM.json,STAR_Spring14dr-STARS14_POSTLS170STAR_AODSIM.json
+
+python DrawOverviewStackedPlots.py -n -a User-MINIAOD.json -d STAR_STAR_USER.json,STAR_Spring14STAR_MINIAODSIM.json
 
 curl "http://dashb-cms-prod.cern.ch/dashboard/request.py/condorjobnumbers_individual?sites=All%20T3210&sitesSort=7&jobTypes=&start=&end=&timeRange=lastWeek&granularity=Hourly&sortBy=3&series=All&type=r" > running.png
 curl "http://dashb-cms-prod.cern.ch/dashboard/request.py/condorjobnumbers_individual?sites=All%20T3210&sitesSort=7&jobTypes=&start=&end=&timeRange=lastWeek&granularity=Hourly&sortBy=3&series=All&type=p" > pending.png
