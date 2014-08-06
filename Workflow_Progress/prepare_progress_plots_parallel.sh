facc542df3543dccbabfb45a38d0213dc375b27f
Oliver Gutsche (oguatworld) <oguatworld@gmail.com>
2014-08-06 09:26:07 +0200

    synchronize execution scripts

--- Workflow_Progress/prepare_progress_plots_parallel.sh
+++ Workflow_Progress/prepare_progress_plots_parallel.sh
@@ -22,23 +22,26 @@ python EventsPerDay.py -d '/*/Spring14*/MINIAODSIM' -s 2014-06-20 &
 python EventsPerDay.py -m -d /*/*/USER -u https://cmsweb.cern.ch/dbs/prod/phys03/DBSReader -s 2014-06-20 &
 
 python EventsPerDay.py -d '/*/*Upg14*/AODSIM' -s 2014-04-27 &
+python EventsPerDay.py -d '/*/*Upg14*PU50bx25*/AODSIM' -s 2014-04-27
+python EventsPerDay.py -d '/*/*Upg14*PU140bx25*/AODSIM' -s 2014-04-27
 python EventsPerDay.py -d '/*/*Upg14*/GEN-SIM-RECO' -s 2014-04-30 &
 python EventsPerDay.py -d '/*/*Upg14*/GEN-SIM' -s 2014-04-20 &
 
-python EventsPerDay.py -d '/*/Summer12*/AODSIM' -s 2012-12-04 &
 python EventsPerDay.py -d '/*/Summer11*/AODSIM' -s 2013-07-04 &
-
 python EventsPerDay.py -d '/*/Summer11*/GEN-SIM' -s 2013-02-24 & 
 
 python EventsPerDay.py -d '/*/*/GEN-SIM' -s 2014-04-05 &
 python EventsPerDay.py -d '/*/*/AODSIM' -s 2014-04-05 &
 
+python EventsPerDay.py -d '/*/Summer12*/AODSIM' -s 2012-12-04
 python EventsPerDay.py -d '/*/Summer12*/GEN-SIM' -s 2012-12-06
 
 python addJSONFiles.py -n User-MINIAOD -d STAR_STAR_USER.json,STAR_Spring14STAR_MINIAODSIM.json
 
 python DrawStackedPlot.py -d STAR_Fall13-STAR_GEN-SIM.json
 python DrawStackedPlot.py -d STAR_STARUpg14STAR_AODSIM.json
+python DrawStackedPlot.py -d STAR_STARUpg14STARPU50bx25STAR_AODSIM.json
+python DrawStackedPlot.py -d STAR_STARUpg14STARPU140bx25STAR_AODSIM.json
 python DrawStackedPlot.py -d STAR_STARUpg14STAR_GEN-SIM-RECO.json
 python DrawStackedPlot.py -d STAR_STARUpg14STAR_GEN-SIM.json
 python DrawStackedPlot.py -d STAR_STAR_AODSIM.json
@@ -61,6 +64,8 @@ python DrawOverviewStackedPlots.py -a STAR_STAR_AODSIM.json -d STAR_Spring14dr-S
 python DrawOverviewStackedPlots.py -a STAR_STAR_AODSIM.json -d STAR_Spring14dr-STAR_AODSIM.json,STAR_STARUpg14STAR_AODSIM.json,STAR_Summer12STAR_AODSIM.json,STAR_Summer11STAR_AODSIM.json -s valid
 
 python DrawOverviewStackedPlots.py -a STAR_Spring14dr-STAR_AODSIM.json -d STAR_Spring14dr-STARPU40bx25_POSTLS170STAR_AODSIM.json,STAR_Spring14dr-STARPU20bx25_POSTLS170STAR_AODSIM.json,STAR_Spring14dr-STARS14_POSTLS170STAR_AODSIM.json
+
+python DrawOverviewStackedPlots.py -a STAR_STARUpg14STAR_AODSIM.json -d STAR_STARUpg14STARPU50bx25STAR_AODSIM.json,STAR_STARUpg14STARPU140bx25STAR_AODSIM.json
 
 python DrawOverviewStackedPlots.py -n -a User-MINIAOD.json -d STAR_STAR_USER.json,STAR_Spring14STAR_MINIAODSIM.json
 
