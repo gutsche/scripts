27322a9cbd1341069126a7c99c0ca0975d7c174c
Oliver Gutsche (oguatworld) <oguatworld@gmail.com>
2018-03-04 18:26:20 -0600

    rename TV shows

---
+++ Misc/rename_tv_shows.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+rename -S ".Chronicles.1080p.AMZN.WEBRip.DD2.0.H265-d3g." "." *
+rename -s ".AMZN.WEB-DL.1080p.AC-3.H265-d3g." "." *
+rename -s ".720p.WEB-DL.H264-NTS." "." *
+rename -s ".720p.WEB-DL.AAC2.H.264-NOGRP." "." *
+rename -s ".720p.webrip.x264-nts." "." *
+rename -s ".720p.web-dl.h264-nts." "." *
+rename -s ".WEBRip.720p.AAC2.0.H.264-Zee." "." *
+rename -s ".720p.AAC2.0.H264-monkee." "." *
+rename -s ".720p.WEBRip.AAC2.0.x264-monkee." "." *
+rename -s ".720p.WEBRip.AAC2. 0.x264-monkee." "." *
+rename -s ".720p.WEBEip.x264." "." *
+rename -s ".720p.WEBRip.x264." "." *
+rename -s ".720p.WEBRip.AAC2.0.x264." "." *
+rename -s ".720p.WEBRip.AAC2.0.x264." "." *
+rename -S ".720p.CCGC.WEBRip.AAC2.0.x264-monkee." "." *
+rename -S ".480p.HDTV.nSD.x264.NhaNc3." "." *
+rename -S ".480p.HDTV.nSD.x264.NhaNc3." "." *
+rename -S ".720pL4.1.MAGiC." "." *
+rename -S ".REAL.720p.HEVC.x265-MeGusta." "." *
+rename -S ".PROPER.720p.HEVC.x265-MeGusta." " " *
+rename -S ".iNTERNAL.720p.HEVC.x265-MeGusta." " " *
+rename -S ".720p.HEVC.x265-MeGusta." "." *
+rename -S ".720p.WEB-DL.DD5.1.H.264-NTb." "." *
+rename -S ".720p.HDTV.x264-KILLERS." " " *
+rename -S ".720p.HDTV.X264-DIMENSION." " " *
+rename -S ".INTERNAL.720p.HDTV.x264-BATV." " " *
+rename -S ".720p.HDTV.x264-IMMERSE." " " *
+rename -S ".INTERNAL.720p.HDTV.x264-FiHTV." " " *
+rename -S ".720p.HDTV.x264-BATV." " " *
+rename -S ".720p.HDTV.x264-0SEC." " " *
+rename -S ".720p.HDTV.x264-FLEET." " " *
+rename -S ".720p.HDTV.x264-AVS." " " *
+rename -S ".REPACK.720p.HDTV.x264-AVS." " " *
+rename -S ".720p.HDTV.x264-SVA." " " *
+rename -S "." " " *
+rename -S " mkv" ".mkv" *
+rename -S " mp4" ".mp4" *
+rename -S " m4v" ".m4v" *
+rename -s " S0" " - S0" *
+rename -s "E01" "E01 -" *
+rename -s "E02" "E02 -" *
+rename -s "E03" "E03 -" *
+rename -s "E04" "E04 -" *
+rename -s "E05" "E05 -" *
+rename -s "E06" "E06 -" *
+rename -s "E07" "E07 -" *
+rename -s "E08" "E08 -" *
+rename -s "E09" "E09 -" *
+rename -s "E10" "E10 -" *
+rename -s "E11" "E11 -" *
+rename -s "E12" "E12 -" *
+rename -s "E13" "E13 -" *
+rename -s "E14" "E14 -" *
+rename -s "E15" "E15 -" *
+rename -s "E16" "E16 -" *
+rename -s "E17" "E17 -" *
+rename -s "E18" "E18 -" *
+rename -s "E19" "E19 -" *
+rename -s "E20" "E20 -" *
+rename -s "E21" "E21 -" *
+rename -s "E22" "E22 -" *
+rename -s "E23" "E23 -" *
+rename -s "E24" "E24 -" *
+rename -s "E25" "E25 -" *
+rename -s "E26" "E26 -" *
+rename -s "E27" "E27 -" *
+rename -s "E28" "E28 -" *
+rename -s "E29" "E29 -" *
+rename -s "E30" "E30 -" *
+rename -S " -.mkv" ".mkv" *
+rename -S " -.mp4" ".mp4" *
+rename -S " -.m4v" ".m4v" *
