e435c1a1aa5457f2c75f9b5c1b7f0d531ba93558
Oliver Gutsche <oguatworld@gmail.com>
2020-01-11 13:56:58 -0600

    script to populate the finder sidebar

---
+++ Misc/populate_sidebar.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# brew cask install mysides
+
+mysides remove Private
+mysides remove Slides
+mysides remove Meetings
+mysides remove Presentations
+mysides remove Library
+mysides remove Screenshots
+mysides remove USCMS
+mysides remove Travel
+mysides remove Work
+mysides remove Scans
+mysides remove Receipts
+mysides remove Desktop
+mysides remove Downloads
+mysides remove gutsche
+
+mysides add Private file:///Volumes/External/Dropbox/Private/
+mysides add Slides file:///Volumes/External/Dropbox/Work/Slides/
+mysides add Meetings file:///Volumes/External/Dropbox/Work/Meetings/
+mysides add Presentations file:///Volumes/External/Dropbox/Work/Presentations/
+mysides add Library file:///Volumes/External/Dropbox/Work/Library/
+mysides add Screenshots file:///Volumes/External/Dropbox/Screenshots/
+mysides add USCMS file:///Volumes/External/Dropbox/Work/Collaborations/CMS/Computing/USCMS/
+mysides add Travel file:///Volumes/External/Dropbox/Work/Travel/
+mysides add Work file:///Volumes/External/Dropbox/Work/
+mysides add Scans file:///Volumes/External/Dropbox/Scans/
+mysides add Receipts file:///Volumes/External/Dropbox/Private/Receipts/
+mysides add Desktop file:///Users/gutsche/Desktop/
+mysides add Downloads file:///Volumes/External/Dropbox/Downloads/
+mysides add gutsche file:///Users/gutsche/
