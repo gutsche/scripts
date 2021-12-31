79af356f43a4ff1f16a4e1aa684459d52e98980e
Oliver Gutsche <oguatworld@gmail.com>
2021-12-31 12:23:49 -0600

    adapted to default Dropbox location

--- Misc/populate_sidebar.sh
+++ Misc/populate_sidebar.sh
@@ -17,18 +17,18 @@ mysides remove Desktop
 mysides remove Downloads
 mysides remove gutsche
 
-mysides add Private file:///Volumes/External/Dropbox/Private/
-mysides add Notes file:///Volumes/External/Dropbox/Notes
-mysides add Slides file:///Volumes/External/Dropbox/Work/Slides/
-mysides add Meetings file:///Volumes/External/Dropbox/Work/Meetings/
-mysides add Presentations file:///Volumes/External/Dropbox/Work/Presentations/
-mysides add Library file:///Volumes/External/Dropbox/Work/Library/
-mysides add Screenshots file:///Volumes/External/Dropbox/Screenshots/
-mysides add USCMS file:///Volumes/External/Dropbox/Work/Collaborations/CMS/Computing/USCMS/
-mysides add Travel file:///Volumes/External/Dropbox/Work/Travel/
-mysides add Work file:///Volumes/External/Dropbox/Work/
-mysides add Scans file:///Volumes/External/Dropbox/Scans/
-mysides add Receipts file:///Volumes/External/Dropbox/Private/Receipts/
+mysides add Private file:///Users/gutsche/Dropbox/Private/
+mysides add Notes file:///Users/gutsche/Dropbox/Notes
+mysides add Slides file:///Users/gutsche/Dropbox/Work/Slides/
+mysides add Meetings file:///Users/gutsche/Dropbox/Work/Meetings/
+mysides add Presentations file:///Users/gutsche/Dropbox/Work/Presentations/
+mysides add Library file:///Users/gutsche/Dropbox/Work/Library/
+mysides add Screenshots file:///Users/gutsche/Dropbox/Screenshots/
+mysides add USCMS file:///Users/gutsche/Dropbox/Work/Collaborations/CMS/Computing/USCMS/
+mysides add Travel file:///Users/gutsche/Dropbox/Work/Travel/
+mysides add Work file:///Users/gutsche/Dropbox/Work/
+mysides add Scans file:///Users/gutsche/Dropbox/Scans/
+mysides add Receipts file:///Users/gutsche/Dropbox/Private/Receipts/
 mysides add Desktop file:///Users/gutsche/Desktop/
-mysides add Downloads file:///Volumes/External/Dropbox/Downloads/
+mysides add Downloads file:///Users/gutsche/Dropbox/Downloads/
 mysides add gutsche file:///Users/gutsche/
